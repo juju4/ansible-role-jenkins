cd499799a1cda803123da4ca712bb5df9a7216db
juju4 <juju4@users.noreply.github.com>
2017-02-26 17:54:05 -0500

    centos+docker: add initscripts, groovy add user possible issue - be more explicit

--- templates/basic-security.groovy
+++ templates/basic-security.groovy
@@ -6,10 +6,15 @@ def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 def users = hudsonRealm.getAllUsers()
 users_s = users.collect { it.toString() }
+/* can conflict with jenkins initial setup:
+   'Jenkins initial setup is required. An admin user has been created and a password generated.'
+   will results in 'Admin user already exists' but not with the right password
+*/
+
 if ("{{ jenkins_admin_username }}" in users_s) {
-    println "Admin user already exists"
+    println "Admin user '{{ jenkins_admin_username }}' already exists"
 } else {
-    println "--> creating local admin user"
+    println "--> creating local admin user: {{ jenkins_admin_username }}"
 
     hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
     instance.setSecurityRealm(hudsonRealm)
